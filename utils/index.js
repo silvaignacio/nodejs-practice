2364effc8c430edce78517fc3bb2d41d307d5f7f
Ignacio Silva Silva <isilva@seiza-ti.cl>
2021-05-09 20:27:14 -0400

    Fix project

---
+++ utils/index.js
@@ -0,0 +1,21 @@
+const PropertyError = require("../exceptions/property-error");
+const readUserData = require("./read-user-data");
+
+/**
+ * 
+ * @param {*} dataUser 
+ * @returns Card Object created from Data Input User
+ */
+const createCard = (dataUser) => {
+    try {
+        
+        return readUserData(dataUser);
+     } catch (error) {
+         if(error instanceof PropertyError) {
+             console.error(error.message);
+         }
+     }
+}
+
+module.exports = createCard;
+
